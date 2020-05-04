5c3e5958840f9b90d9896faa326266a2d678d9b2
Andrea Bucci <andrea.bucci@unich.it>
2020-05-04 13:40:02 +0000

    version 0.1.7

---
+++ R/VLSTARjoint.R
@@ -0,0 +1,46 @@
+VLSTARjoint <- function(y1, x1, st, alpha = 0.05){
+  y <- as.matrix(y1)
+  x <- as.matrix(x1)
+  nrowx <- nrow(x)
+  ncolx <- ncol(x)
+  ncoly <- ncol(y)
+  q <- ncolx-ncoly
+
+  ##VAR Estimation Y on X
+varest <- stats::lm(y~x)
+ee <- stats::residuals(varest)
+RSS0 <- t(ee)%*%ee
+ZZ <- matrix(nrow = nrowx, ncol = ncolx*3)
+for (i in 1:nrowx){
+  xst1 <-  as.matrix(x[i,]*st[i])
+  xst2 <- as.matrix(x[i,]*st[i]^2)
+  xst3 <- as.matrix(x[i,]*st[i]^3)
+ ZZ[i,] <- cbind(xst1, xst2, xst3)
+}
+ausvar <- stats::lm(ee ~ ZZ)
+ll <- stats::residuals(ausvar)
+RSS1 <- t(ll)%*%ll
+trac1 <- matrixcalc::matrix.trace(MASS::ginv(RSS0)%*%RSS1)
+LM3 <- nrowx*(ncoly - trac1)
+df <- 3*ncoly*(q+ncoly)
+conflev <- 1-alpha/2
+chi <- stats::qchisq(conflev, df)
+pvalue <- stats::pchisq(LM3, df, lower.tail=FALSE)
+results <- list(LM3, pvalue, chi)
+names(results) <- c('LM', 'pval', 'critical')
+class(results) = 'VLSTARjoint'
+#cat('Joint linearity test (Third-order Taylor expansion)\n')
+return(results)
+}
+
+
+
+#' @S3method print.VLSTARjoint
+print.VLSTARjoint <- function(x, ...)
+{
+  digits = 3
+  cat("\nJoint linearity test (Third-order Taylor expansion)\n")
+  cat(" LM =", format(x$LM, digits=digits),"; p-value =", format(x$pval, digits=digits),"\n")
+  cat(" Critical value for alpha =", format(x$critical, digits=digits), "\n")
+  invisible(x)
+}
---
+++ R/predict.VLSTAR.R
@@ -0,0 +1,36 @@
+#'@S3method predict VLSTAR
+
+predict.VLSTAR <- function(object, ...){
+  k <- ncol(object$Data[[2]])
+  alpha = 0.05
+  if(object$exo == FALSE){
+    newdata <- as.matrix(object$Data[[1]][(nrow(object$Data[[1]])-(object$p-1)):(nrow(object$Data[[1]])),])
+  }else{
+   newdata <- as.matrix(c(object$Data[[1]][(nrow(object$Data[[1]])-(object$p-1)):(nrow(object$Data[[1]])),], as.matrix(newdata)))
+  }
+
+  if(!missing(newdata)) {
+    if(!inherits(newdata, c("data.frame", "matrix","zoo", "ts"))) stop("Arg 'newdata' should be of class data.frame, matrix, zoo or ts")
+    if(!dim(newdata)[1]== (k-1))stop("newdata should have the same dimension of the design matrix (p*n+k)")}
+  ## extract parameters, coefs
+  if(object$constant == TRUE){
+    newdata <- rbind(1, newdata)
+  }
+  newdata <- as.matrix(newdata)
+
+  BB <- object$B
+  In <- diag(ncol(object$Data[[1]]))
+  Gtilde <- t(cbind(In, object$Gtilde))
+  pred <- as.data.frame((t(Gtilde)%*%t(BB)%*%newdata))
+  rownames(pred) <- colnames(object$Bhat)
+  colnames(pred) <- 'Prediction'
+  pred$LB <- NA
+  pred$UB <- NA
+  for(i in 1:ncol(object$Data[[1]])){
+   std_err <- sqrt(object$Omega[i,i])*t(newdata)%*%MASS::ginv(t(object$Data[[2]])%*%object$Data[[2]])%*%newdata
+     pred$LB[i] <- pred$Prediction[i]-stats::qt((1-alpha), df = nrow(object$Data[[1]])*ncol(object$Data[[1]])-length(BB))*std_err
+     pred$UB[i] <- pred$Prediction[i]+stats::qt((1-alpha), df = nrow(object$Data[[1]])*ncol(object$Data[[1]])-length(BB))*std_err
+  }
+
+  return(pred)
+}
